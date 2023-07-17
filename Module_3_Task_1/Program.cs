2de7b3cd17fc56d75e6e94fc9a999bf54be2d271
inga-paladi <ingapaladi98@gmail.com>
2023-07-17 07:59:55 -0700

    Module 3. Task 1

---
+++ Module_3_Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Module_3_Task_1
+{
+    public static class MainClass
+    {
+        public static void Main()
+        {
+            Employee employee1 = new Employee("John", 1500, "QA");
+            Employee employee2 = new Employee("Alice", 2000, "Development");
+            Employee employee3 = new Employee("Bob", 1800, "HR");
+
+            employee1.ShowDetails();
+            employee2.ShowDetails();
+            employee3.ShowDetails();
+        }
+    }
+}
